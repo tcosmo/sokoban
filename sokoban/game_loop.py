c88ab42cd38bb33145d5b04390644b03d9ebaa71
Cosmo <tristan.sterin@gmail.com>
2021-01-22 18:26:10 +0000

    Setting up the game loop

--- sokoban/game_loop.py
+++ sokoban/game_loop.py
@@ -1,4 +1,18 @@
 import pygame
 
 def run():
-    print("coucou")
+    pygame.init()
+    pygame.key.set_repeat(150)
+
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption('Sokoban!!')
+
+    running = True
+    while running:  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+        screen.fill((0, 0, 0))
+    
+        pygame.display.flip()
